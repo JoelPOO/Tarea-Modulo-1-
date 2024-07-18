74e4c111dfe65e5e0888a499144ff28bfb21972b
JoelPOO <jo.herrera2590@gmail.com>
2024-07-18 15:48:59 -0600

    Add files via upload

---
+++ Operaciones.java
@@ -0,0 +1,34 @@
+
+
+package com.mycompany.operaciones;
+
+public class Operaciones {
+
+    public static void main(String[] args) {
+        //SUMA
+        int n1=10, n2=20, suma;
+        suma=n1+n2;
+        //RESTA
+        int r1=10, r2=20, resta;
+        resta=r1-r2;
+        //DIVICION
+        double d1=10.25, d2=3, div, multi;
+        div=d1/d2;
+        // MULTIPLICACION
+        multi=d1*d2;
+        // BARIABLE TIPO STRING
+        String nombre="Joel";
+         System.out.println(" la suma es:"+suma+"\nla resta es:"+resta+"\nla divicion es:"+ div+" \nla multiplicacions es:"+multi);
+          System.out.println("Mi nombre es "+nombre);
+        
+        
+        
+        
+      
+   
+        
+        
+        
+       
+    }
+}
