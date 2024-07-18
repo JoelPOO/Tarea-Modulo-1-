0ffd591981a2978616fc25228493d4c076378041
JoelPOO <jo.herrera2590@gmail.com>
2024-07-18 14:32:12 -0600

    Add files via upload

---
+++ Arreglo1.java
@@ -0,0 +1,30 @@
+
+
+package com.mycompany.arreglo1;
+
+//import.java.util.Arrays;
+public class Arreglo1 {
+
+    public static void main(String[] args) {
+        String[] nombre=new String[10];
+        nombre[0]="Diego Peña";
+         nombre[1]="Cindy Salinas";
+         nombre[2]="Sarita Li";
+         nombre[3]="Luis Aleman";
+         nombre[4]="Luis Reyes";
+         nombre[5]="Joel Sarmiento";
+         nombre[6]="Jose Lopes";
+         nombre[7]="Carlos Estrada";
+         nombre[8]="Elbin Mendez";
+         nombre[9]= "Aminta erazo" ; 
+          System.out.println("Los nombres de mis compañeros son:");
+        for(int i=0;i<nombre.length; i++){
+        System.out.println(nombre[i]+ " ");
+    }
+    }
+
+}
+   //{"Diego Peña",""","Sarita Li","Luis Aleman","Luis Reyes","Joel Sarmiento","Jose Lopes","Carlos Estrada","Elbin Mendez","Aminta erazo"};
+        //System.out.println(Arrays.to 
+
+//Crear un arrelo que guarde e imprima 10 nombres de tus compañeros de la clase
